46dcfa093520da03d9bf60a3617433a6a435781f
Slobodan Sesto <Slobodan.600000@gmail.com>
2019-10-16 23:30:46 +0200

    testing auth route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,49 @@
 const User = require('../models/User');
+const db = require('../db');
 
 exports.getUser = ((req, res) => {
-    let u = new User(1, 'Bob Smith', 'email@email.com');
-    res.send(u);
+	let u = new User(1, 'Bob Smith', 'email@email.com');
+	res.send(u);
+});
+
+exports.authUser = ((req, res) => {
+	let email = req.query.email || null;
+	let pass = req.query.pass || null;
+	// console.log(req.query.sid);
+	if(email && pass) {
+		db.connect((err) => {
+			if (err) {
+				throw err;
+			};
+			console.log('connection opened');
+		});
+		let sql = `SELECT * FROM users WHERE usr_email=?`;
+		db.query(sql, email, (err, results) => {
+			// console.log(results.length);
+			if(results && results.length > 0) {
+				// check pass
+				console.log(results[0].usr_password);
+				if(results[0].usr_password == pass) {
+					// open session 
+					console.log("all good");
+					// send sid along with user info
+					let data = {
+						"data": results,
+						"status": "OK"
+					};
+					res.send(data);
+				} else {
+					res.send('Wrong Pass');
+				}
+			} else {
+				res.send('no results from db');
+			}
+			db.end((error) => {
+				if(error) { throw error };
+				console.log('connection closed');
+			});
+		});
+	} else {
+		res.send('params missing');
+	}
 });
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const userController = require('../controllers/userController');
 
 router.post('/login', (req, res) => {
-	res.send("got it");
+	return userController.authUser(req, res);
+	// res.send(`{"status":"OK","login":"success","sid":"zof7PbhHQUEW2vGTTBenbMQWQoPoAmNg","user":{"usr_id":63,"name":"Test","surname":"Testic","email":"Test@gmail.com","getemail":0,"utc":null,"fcm":null}}`);
 });
 
 module.exports = router;
